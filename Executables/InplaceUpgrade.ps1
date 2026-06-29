bf04cd8f6d096bf7fb0e092d810ceac139d634d9
Kiyomizu <kiyomizu.suzu@gmail.com>
2026-06-29 20:36:05 +0900

    Updated to v3.0.0 Stable

--- Executables/InplaceUpgrade.ps1
+++ Executables/InplaceUpgrade.ps1
@@ -1,50 +1,73 @@
+param([ValidateSet('Startup', 'OOBE')][string]$Mode)
 Import-Module "$PSScriptRoot\Module.psm1"
 Initialize-RuntimeDefaults
 # ============================================================================
 # CONFIGURATION
 # ============================================================================
+$UpgradeKey = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\Setup\Upgrade'
 $LocalAccounts = (Get-LocalUser).Where({
-    $_.Name -notin @(
-        'Administrator',
-        'DefaultAccount',
-        'Guest',
-        'WDAGUtilityAccount',
-        'defaultuser0'
-    )
-})
+                $_.Name -notin @(
+                    'Administrator',
+                    'DefaultAccount',
+                    'Guest',
+                    'WDAGUtilityAccount',
+                    'defaultuser0'
+                )
+            })
 # ============================================================================
 # EXECUTION
 # ============================================================================
-if ($LocalAccounts.Count -eq 0) {
-    Write-Log "No Local Account was made yet."
-}
-else {
-    Write-Log "Found $($LocalAccount.Name -join ', ')" 'WARN'
-    $lightTheme = ((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\AME').UseLightTheme -eq 1) ? 1 : 0
-    foreach ($account in $LocalAccounts) {
-        $sid = $account.SID.Value
-        Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
-            -Name 'AppsUseLightTheme' `
-            -Type 'DWord' `
-            -Value $lightTheme `
-            -Desc 'App theme mode'
-        Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
-            -Name 'SystemUsesLightTheme' `
-            -Type 'DWord' `
-            -Value $lightTheme `
-            -Desc 'System theme mode'
+switch ($Mode) {
+    'Startup' {
+        $UpgradeKeyExists = Test-Path -Path $UpgradeKey
+        $winLogonPath = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+        $currentShell = (Get-ItemProperty -Path $winLogonPath -Name 'Shell').Shell
+        if ($UpgradeKeyExists) {
+            $service = Get-Service 'ameoobe'
+            if ($service.Status -ne 'Running') {
+                Start-Service $service
+            }
+            $lightTheme = ((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\AME').UseLightTheme -eq 1) ? 1 : 0
+            Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
+                -Name 'AppsUseLightTheme' `
+                -Type 'DWord' `
+                -Value $lightTheme `
+                -Desc 'App theme mode'
+            Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
+                -Name 'SystemUsesLightTheme' `
+                -Type 'DWord' `
+                -Value $lightTheme `
+                -Desc 'System theme mode'
+        }
+        elseif ($currentShell -ne 'explorer.exe') {
+            Write-Log "InPlaceUpgrade detected! Restoring explorer.exe for you..."
+            Set-RegistryValue -Path $path `
+                -Name 'Shell' `
+                -Type 'String' `
+                -Value 'explorer.exe' `
+                -Desc 'Set explorer.exe as shell'
+            Start-Process explorer.exe
+        }
     }
-    Set-registryValue -path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' `
-        -Name 'EnableFirstLogonAnimation' `
-        -Type: 'DWord' `
-        -Value: 0 `
-        -Desc 'Remove the animation setting up new user'
-    Set-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' `
-        -Name 'Shell' `
-        -Type 'String' `
-        -Value 'explorer.exe' `
-        -Desc 'Set explorer.exe as shell'
-    Remove-RegistryKey -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\ameoobe' `
-        -Desc 'Remove ameoobe service'
-    Restart-Computer -Force
+    'OOBE' {
+        if ($LocalAccounts.Count -eq 0) {
+            Write-Log "No Local Account was made yet."
+            Register-ScheduledTask -TaskName "AME Upgrade" `
+                -Action (New-ScheduledTaskAction -Execute "C:\Program Files\PowerShell\7\pwsh.exe" `
+                -Argument '-NoProfile -ExecutionPolicy Bypass -File "C:\ProgramData\AME\ActiveSetup\InplaceUpgrade.ps1" -Mode Startup') `
+                -Trigger (New-ScheduledTaskTrigger -AtLogOn) `
+                -Principal (New-ScheduledTaskPrincipal -GroupId "S-1-5-4" -RunLevel Highest) `
+                -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries)
+        }
+        else {
+            Write-Log "Found $($LocalAccounts.Name -join ', ')" 'WARN'
+            Set-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' `
+                -Name 'EnableFirstLogonAnimation' `
+                -Type 'DWord' `
+                -Value 0 `
+                -Desc 'Remove the animation setting up new user'
+            Remove-RegistryKey -Path $UpgradeKey `
+                -Desc 'Reset upgrade state'
+        }
+    }
 }
--- Executables/LiveSystem.ps1
+++ Executables/LiveSystem.ps1
@@ -3,157 +3,155 @@ Initialize-RuntimeDefaults
 # ============================================================================
 # CONFIGURATION
 # ============================================================================
-$username = (((quser) -match '^>') -replace '^>', '').Trim().Split()[0]
-$sid = ([System.Security.Principal.NTAccount]"$env:COMPUTERNAME\$username").Translate([System.Security.Principal.SecurityIdentifier]).Value
 $lightTheme = ((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\AME').UseLightTheme -eq 1) ? 1 : 0
 $wallpaperFolder = 'C:\Windows\Web\Wallpaper\Windows'
 $wallpaperFile = ($lightTheme -eq 1) ? "$wallpaperFolder\img0.jpg" : "$wallpaperFolder\img19.jpg"
 # ============================================================================
 # EXECUTION
 # ============================================================================
-Set-BinaryBit -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-BinaryBit -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'UserPreferencesMask' `
     -ByteIndex 4 `
     -BitMask 0x02 `
     -SetBit $False `
     -Desc 'System UI animations'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop\WindowMetrics" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop\WindowMetrics" `
     -Name 'MinAnimate' `
     -Type 'String' `
     -Value 0 `
     -Desc 'Window minimize/maximize animation'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
     -Name 'TaskbarAnimations' `
     -Type 'DWord' `
     -Value 0 `
     -Desc 'Taskbar animations'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\DWM" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\DWM" `
     -Name 'EnableAeroPeek' `
     -Type 'DWord' `
     -Value 1 `
     -Desc 'Desktop peek feature'
-Set-BinaryBit -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-BinaryBit -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'UserPreferencesMask' `
     -ByteIndex 0 `
     -BitMask 0x02 `
     -SetBit $False `
     -Desc 'Menu animations'
-Set-BinaryBit -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-BinaryBit -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'UserPreferencesMask' `
     -ByteIndex 1 `
     -BitMask 0x08 `
     -SetBit $False `
     -Desc 'Tooltip animations'
-Set-BinaryBit -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-BinaryBit -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'UserPreferencesMask' `
     -ByteIndex 1 `
     -BitMask 0x04 `
     -SetBit $False `
     -Desc 'Menu fade effects'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\DWM" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\DWM" `
     -Name 'AlwaysHibernateThumbnails' `
     -Type 'DWord' `
     -Value 0 `
     -Desc 'Taskbar thumbnail caching'
-Set-BinaryBit -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-BinaryBit -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'UserPreferencesMask' `
     -ByteIndex 1 `
     -BitMask 0x20 `
     -SetBit $True `
     -Desc 'Mouse cursor shadow'
-Set-BinaryBit -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-BinaryBit -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'UserPreferencesMask' `
     -ByteIndex 2 `
     -BitMask 0x04 `
     -SetBit $False `
     -Desc 'Window shadows'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
     -Name 'IconsOnly' `
     -Type 'DWord' `
     -Value 0 `
     -Desc 'File preview vs icons'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
     -Name 'ListviewAlphaSelect' `
     -Type 'DWord' `
     -Value 0 `
     -Desc 'Selection box transparency'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'DragFullWindows' `
     -Type 'String' `
     -Value 1 `
     -Desc 'Show window contents while dragging'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'FontSmoothing' `
     -Type 'String' `
     -Value 2 `
     -Desc 'Font anti-aliasing'
-Set-BinaryBit -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-BinaryBit -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'UserPreferencesMask' `
     -ByteIndex 0 `
     -BitMask 0x04 `
     -SetBit $False `
     -Desc 'Combo box animation'
-Set-BinaryBit -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-BinaryBit -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'UserPreferencesMask' `
     -ByteIndex 0 `
     -BitMask 0x08 `
     -SetBit $False `
     -Desc 'Smooth scrolling'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
     -Name 'ListviewShadow' `
     -Type 'DWord' `
     -Value 0 `
     -Desc 'Desktop icon text shadow'
 
 
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Search" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" `
     -Name 'SearchboxTaskbarMode' `
     -Type 'DWord' `
     -Value 1 `
     -Desc 'Choose how Windows search appears on the taskbar: hidden, icon only, icon with label, or full search box'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" `
     -Name '(Default)' `
     -Type 'String' `
     -Value '' `
     -Desc 'Use the Windows 10-style right-click menu with all options visible instead of the simplified Windows 11 menu'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Control Panel\Accessibility\HighContrast" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Control Panel\Accessibility\HighContrast" `
     -Name 'Flags' `
     -Type 'String' `
     -Value 4194 `
     -Desc 'High contrast shortcut'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Classes\CLSID\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Classes\CLSID\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}" `
     -Name 'System.IsPinnedToNameSpaceTree' `
     -Type 'DWord' `
     -Value 0 `
     -Desc 'Show Home folder'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Classes\CLSID\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Classes\CLSID\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}" `
     -Name 'System.IsPinnedToNameSpaceTree' `
     -Type 'DWord' `
     -Value 0 `
     -Desc 'Show Gallery folder'
 
 
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
     -Name 'AppsUseLightTheme' `
     -Type 'DWord' `
     -Value $lightTheme `
     -Desc 'App theme mode'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
     -Name 'SystemUsesLightTheme' `
     -Type 'DWord' `
     -Value $lightTheme `
     -Desc 'System theme mode'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'WallpaperStyle' `
     -Type 'String' `
     -Value 10 `
     -Desc 'Wallpaper mode'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'TileWallpaper' `
     -Type 'String' `
     -Value 0 `
     -Desc 'Choose a fit for the desktop image'
-Set-RegistryValue -Path "Registry::HKEY_USERS\$sid\Control Panel\Desktop" `
+Set-RegistryValue -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" `
     -Name 'WallPaper' `
     -Type 'String' `
     -Value $wallpaperFile `
